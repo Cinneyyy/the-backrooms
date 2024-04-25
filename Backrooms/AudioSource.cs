ed17d28862381d4ed3fda1f7cc03e37b471f3c3e
Cinneyyy <Colinjunior1@web.de>
2024-04-25 17:54:52 +0200

    unnecessery commit please ignore

---
+++ Backrooms/AudioSource.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows.Media;
+using System.Media;
+using System.Runtime.InteropServices;
+using System;
+
+namespace Backrooms;
+
+public partial class AudioSource(string audioName)
+{
+    public bool playing;
+    public bool loop;
+    public float volume;
+
+    // [potential] todo: give each AudioSource a unique SoundPlayer, instead of the same one, such that multiple instances of the same sound can be played and managed at once
+    private readonly SoundPlayer soundPlayer = Resources.audios[audioName];
+
+
+    public void Play()
+    {
+        SetWaveOutVolume(volume);
+        if(loop) soundPlayer.PlayLooping();
+        else soundPlayer.Play();
+    }
+
+    public void Stop()
+    {
+    }
+
+
+    public static void SetWaveOutVolume(float volume)
+    {
+        uint vol = (uint)(volume * 10_000f);
+        _ = waveOutSetVolume(nint.Zero, (vol & 0xffff) | (vol << 0xf));
+    }
+    public static float GetWaveOutVolume()
+    {
+        _ = waveOutGetVolume(nint.Zero, out uint dwVolume);
+        return (dwVolume & 0xffff) / 10_000f;
+    }
+
+
+    [LibraryImport("winmm.dll")]
+    private static partial int waveOutSetVolume(IntPtr hwo, uint dwVolume);
+
+    [LibraryImport("winmm.dll")]
+    private static partial int waveOutGetVolume(IntPtr hwo, out uint dwVolume);
+}
--- Backrooms/Resources.cs
+++ Backrooms/Resources.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Drawing;
 using System.IO;
+using System.Media;
 using System.Reflection;
 
 namespace Backrooms;
@@ -12,23 +14,28 @@ public static class Resources
         Image,
         Font,
         Icon,
+        Audio,
         Unknown
     }
 
 
     public static readonly Dictionary<string, Image> sprites = [];
     public static readonly Dictionary<string, Icon> icons = [];
+    public static readonly Dictionary<string, SoundPlayer> audios = [];
+
+    private static readonly StringDictionary manifests = [];
+    private static readonly Assembly assembly = Assembly.GetExecutingAssembly();
 
 
     static Resources()
     {
-        Assembly assembly = Assembly.GetExecutingAssembly();
         foreach(string manifest in assembly.GetManifestResourceNames())
         {
             ResType type = Path.GetExtension(manifest) switch {
                 ".png" or ".jpg" or ".jpeg" => ResType.Image,
                 ".ttf" or ".otf" => ResType.Font,
                 ".ico" => ResType.Icon,
+                ".mp3" or ".wav" or ".ogg" => ResType.Audio,
                 _ => ResType.Unknown
             };
 
@@ -36,13 +43,23 @@ public static class Resources
                 continue;
 
             string name = manifest.Split('.')[^2];
-            Stream stream = assembly.GetManifestResourceStream(manifest);
+            manifests.Add(name, manifest);
+
+            using Stream stream = assembly.GetManifestResourceStream(manifest);
             switch(type)
             {
                 case ResType.Image: sprites.Add(name, Image.FromStream(stream)); break;
                 case ResType.Icon: icons.Add(name, new(stream)); break;
+                case ResType.Audio: 
+                    audios.Add(name, new(stream)); 
+                    audios[name].Load();
+                    break;
                 default: break;
             }
         }
     }
+
+
+    public static Stream GetStream(string resourceName)
+        => assembly.GetManifestResourceStream(manifests[resourceName]);
 }
